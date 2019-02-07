412325b59ccfa1876cfc2f362eef1ec0fd4d88a4
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-06 22:41:47 -0200

    input component

---
+++ src/Components/Input/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import st from './style.js';
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <label style={st.label}>{this.props.placeholder}</label>
+                <input type={this.props.type} style={st.input}></input>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Input/style.js
@@ -0,0 +1,18 @@
+export default {
+    label: {
+        color: 'white',
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '14px',
+        fontWeight: "bold",
+    },
+    input: {
+        borderBottom: "solid 2px white",
+        borderTop: "0px",
+        borderLeft: "0px",
+        borderRight: "0px",
+        background: 'transparent',
+        width: "100%",
+        color: 'white',
+        fontFamily: 'sans-serif',
+    },
+}
