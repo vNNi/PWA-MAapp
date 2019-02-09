0d084017e842b19bbb5e18af6b1b25958ed0315f
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-09 00:40:27 -0200

    text and image to location page

--- src/CopyDeck.js
+++ src/CopyDeck.js
@@ -1,4 +1,5 @@
 export default {
     yourSecurity: "Sua segurança é nossa maior vitória!",
     dontHasAccount: "Ainda não tem conta? Cadastre-se",
+    doCheckinOrCheckout: "Diga onde você está!"
 }
