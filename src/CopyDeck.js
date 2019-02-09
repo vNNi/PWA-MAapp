803143d501a5f0918f6c5f51068db361ce396d7c
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-09 21:54:44 -0200

    text to copy deck

--- src/CopyDeck.js
+++ src/CopyDeck.js
@@ -1,5 +1,7 @@
 export default {
     yourSecurity: "Sua segurança é nossa maior vitória!",
     dontHasAccount: "Ainda não tem conta? Cadastre-se",
-    doCheckinOrCheckout: "Diga onde você está!"
+    doCheckinOrCheckout: "Diga onde você está!",
+    checkList: "Últimas localizações",
+    listSubtitle: "Second text to explain something",
 }
