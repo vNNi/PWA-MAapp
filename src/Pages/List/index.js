851e6c6bc45c544fc8436a6feb6c4b50ebd90fe5
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-10 17:29:45 -0200

    add token verification in list component

--- src/Pages/List/index.js
+++ src/Pages/List/index.js
@@ -4,9 +4,10 @@ import st from './style.js';
 import BottomNav from '../../Components/BottomBar/index';
 import Legend from '../../Components/Legend/index';
 import ItemList from '../../Components/ItemList/index';
+import { Redirect } from 'react-router-dom';
 export default class index extends Component {
     render() {
-        return (
+        const list = (
             <div>
                 <BottomNav />
                 <div style={st.headerContainer}>
@@ -19,6 +20,14 @@ export default class index extends Component {
                 <div style={st.listWrapper}>
                     <ItemList />
                 </div>
+            </div>
+        );
+        const logged = localStorage.getItem('token');
+        return (
+            <div>
+                {
+                    logged ? list : <Redirect to="/" />
+                }
             </div>
         )
     }
