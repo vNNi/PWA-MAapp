66a78cbdb85eb82007ded8654dfb18466113fb9e
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-10 17:30:04 -0200

    api call and handle inputs

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -3,7 +3,30 @@ import st from './style.js';
 import cp from '../../CopyDeck';
 import Input from '../../Components/Input/index';
 import Button from '../../Components/Button/index';
+import APIService from '../../Service/APIService';
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.apiservice = new APIService();
+    }
+    state = {
+        email: '',
+        password: '',
+    }
+    login = (e) => {
+        this.apiservice.login(this.state.email, this.state.password);
+    }
+    handleEmail = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    }
+    handlePassword = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+        console.log('222');
+    }
     render() {
         return (
             <div>
@@ -14,13 +37,15 @@ export default class Login extends Component {
                                 <p>{cp.yourSecurity}</p>
                             </div>
                             <div style={st.inputWrapper}>
-                                <Input type="email" placeholder="E-mail :" />
+                                <Input type="email" placeholder="E-mail :" onChange={this.handleEmail} />
                             </div>
                             <div style={st.inputWrapper}>
-                                <Input type="text" placeholder="Senha :" />
+                                <Input type="text" placeholder="Senha :" onChange={this.handlePassword} />
                             </div>
                             <div style={st.buttonWrapper}>
-                                <Button text="Login" />
+                                <div onClick={this.login}>
+                                    <Button text="Login" />
+                                </div>
                             </div>
                         </div>
                         <div style={st.registerLink}>
