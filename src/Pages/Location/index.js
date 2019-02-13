4bea6dae989db6b3969b39054998f62f71b595c3
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-13 00:00:32 -0200

    add fetch to google api and get location from user

--- src/Pages/Location/index.js
+++ src/Pages/Location/index.js
@@ -3,7 +3,12 @@ import st from './style.js';
 import BottomNav from '../../Components/BottomBar/index';
 import Button from '../../Components/Button/index';
 import cp from '../../CopyDeck';
+import APIService from '../../Service/APIService';
 export default function index() {
+    function checkIn() {
+        const apiservice = new APIService();
+        return apiservice.newLocation(1, "check-in");
+    }
     return (
         <div style={st.imgBd}>
             <BottomNav />
--- src/Service/APIService.js
+++ src/Service/APIService.js
@@ -7,6 +7,8 @@ export default class APIService {
         this.url = "http://localhost:4000";
         this.apiLogin = data[0];
         this.apiPass = data[1];
+        this.GApi = 'https://maps.googleapis.com/maps/api/geocode/';
+        this.GApiKey = 'AIzaSyD35EvA-8rFF2mb5S-RO7B-HXmK9_Pe9MQ';
     }
     login(userEmail, userPassword) {
         const request = {
@@ -73,5 +75,46 @@ export default class APIService {
         }).catch((err) => {
             history.push('/ServerError');
         })
+    }
+    newLocation(code = 0, text = "") {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+                console.log(lat + " " + lng);
+                fetch(`${this.GApi}json?latlng=${lat},${lng}&key=${this.GApiKey}`).then((response) => {
+                    return response.json();
+                }).then((result) => {
+                    const address = result.results[0].address_components[1].short_name;
+                    const number = result.results[0].address_components[0].short_name;
+                    console.log(address + " " + number);
+                    const request = {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+                        },
+                        body: JSON.stringify({
+                            alias: localStorage.getItem('userData').name,
+                            date: new Date(),
+                            status: {
+                                code: code,
+                                text: text,
+                            },
+                            address: `${address} - ${number}`,
+                        }),
+                    }
+                    fetch(`${this.url}/user/location/register`, request).then((response) => {
+                        if (response.ok) {
+                            if (response.status === 201) {
+                                history.push('/list', { newLocation: true });
+                            }
+                        }
+                    });
+                });
+            }, () => {
+
+            })
+        }
     }
 };
