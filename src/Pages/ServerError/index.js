df8ddc3cf830058077d21b52e239e9eff8514b4b
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-13 22:30:39 -0200

    Server error page

---
+++ src/Pages/ServerError/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import st from './style.js';
+import errorImg from '../../Images/sadPhone.svg';
+import CopyDeck from '../../CopyDeck.js';
+import { Link } from 'react-router-dom';
+export default function index() {
+    return (
+        <div style={st.container}>
+            <img src={errorImg} style={st.img} alt="telefone triste por conter error no app" />
+            <span style={st.textError}> {CopyDeck.defaultErrorServer}</span>
+            <Link to="/list" style={st.link}>Clique aqui para voltar</Link>
+        </div>
+    )
+}
---
+++ src/Pages/ServerError/style.js
@@ -0,0 +1,30 @@
+import Colors from "../../Colors";
+export default {
+    container: {
+        backgroundColor: 'black',
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    img: {
+        maxWidth: '60%',
+    },
+    textError: {
+        color: 'white',
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '22px',
+        margin: '2em 5% 0 5%',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    link: {
+        color: Colors.primary,
+        textDecoration: 'none',
+        fontFamily: 'Roboto, serif',
+        fontSize: '16px',
+        marginTop: '1em',
+    }
+}
