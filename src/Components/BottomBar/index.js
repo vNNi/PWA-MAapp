d28e40d1f76312d79320f48efeb65a5b0667a351
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-17 17:27:40 -0300

    add links to navbar

--- src/Components/BottomBar/index.js
+++ src/Components/BottomBar/index.js
@@ -3,18 +3,25 @@ import st from './style.js';
 import listImg from '../../Images/list.svg';
 import locationImg from '../../Images/Component.svg';
 import gearsImg from '../../Images/settings-gears.svg';
+import { Link } from 'react-router-dom'
 export default function index() {
   return (
     <div style={st.container}>
       <div style={st.content}>
         <div style={st.navItem}>
-          <img src={listImg} alt="ver lista!" />
+          <Link to="/list">
+            <img src={listImg} alt="ver lista!" />
+          </Link>
         </div>
         <div style={st.navItemLocation}>
-          <img src={locationImg} alt="fazer check-in ou check-out" />
+          <Link to="/location">
+            <img src={locationImg} alt="fazer check-in ou check-out" />
+          </Link>
         </div>
         <div style={st.navItem}>
-          <img src={gearsImg} alt="fazer check-in ou check-out" />
+          <Link to="/list">
+            <img src={gearsImg} alt="fazer check-in ou check-out" />
+          </Link>
         </div>
       </div>
     </div>
