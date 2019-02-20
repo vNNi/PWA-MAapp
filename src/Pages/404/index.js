f514e7a92458a5059f2df1a62d7a1f365373b5a3
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-19 23:29:18 -0300

    adding nav bar to error pages

--- src/Pages/404/index.js
+++ src/Pages/404/index.js
@@ -3,12 +3,17 @@ import st from './style.js';
 import errorImg from '../../Images/404.svg';
 import CopyDeck from '../../CopyDeck.js';
 import { Link } from 'react-router-dom';
+import BottomNav from '../../Components/BottomBar/index';
+
 export default function index() {
     return (
-        <div style={st.container}>
-            <img src={errorImg} style={st.img} alt="pagina triste" />
-            <span style={st.textError}> {CopyDeck.notFoundPage}</span>
-            <Link to="/list" style={st.link}>Clique aqui para voltar</Link>
+        <div>
+            <div style={st.container}>
+                <img src={errorImg} style={st.img} alt="pagina triste" />
+                <span style={st.textError}> {CopyDeck.notFoundPage}</span>
+                <Link to="/list" style={st.link}>Clique aqui para voltar</Link>
+            </div>
+            <BottomNav/>
         </div>
     )
 }
--- src/Pages/ServerError/index.js
+++ src/Pages/ServerError/index.js
@@ -3,12 +3,16 @@ import st from './style.js';
 import errorImg from '../../Images/sadPhone.svg';
 import CopyDeck from '../../CopyDeck.js';
 import { Link } from 'react-router-dom';
+import BottomNav from '../../Components/BottomBar/index';
 export default function index() {
     return (
-        <div style={st.container}>
-            <img src={errorImg} style={st.img} alt="telefone triste por conter error no app" />
-            <span style={st.textError}> {CopyDeck.defaultErrorServer}</span>
-            <Link to="/list" style={st.link}>Clique aqui para voltar</Link>
+        <div>
+            <div style={st.container}>
+                <img src={errorImg} style={st.img} alt="telefone triste por conter error no app" />
+                <span style={st.textError}> {CopyDeck.defaultErrorServer}</span>
+                <Link to="/list" style={st.link}>Clique aqui para voltar</Link>
+            </div>
+            <BottomNav/>
         </div>
     )
 }
