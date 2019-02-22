c7a78c98c499161ec9ac254b127467ea00626bc0
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-02-21 21:41:22 -0300

    adjust button to be desable

--- src/Components/Button/style.js
+++ src/Components/Button/style.js
@@ -3,11 +3,10 @@ export default {
     button: props => ({
         border: '0',
         fontFamily: 'Roboto, sans-serif',
-        backgroundColor: props.backgroundColor || Colors.primary,
+        backgroundColor: props.disable===true?"grey":(props.backgroundColor || Colors.primary),
         fontWeight: 'bold',
         padding: '10px 30px',
         borderRadius: '4px',
         color: props.color || 'white',
-
     }),
 }
